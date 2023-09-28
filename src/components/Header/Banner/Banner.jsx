40e08ffbb446551fa0638a72c90c10ecf457d59d
Md Masud Rana <masudhcsf@gmail.com>
2023-09-28 02:15:10 -0700

    all done

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,9 +2,8 @@ const Banner = () => {
   return (
 
     <div>
-
-      <h1 className="text-5xl font-bold text-center my-3">I Grow By Helping People In Need</h1>
-      <div className="mt-10 text-center"> <input className="border p-2 px-3 rounded-md" type="text" placeholder="Serch here..." value="" />
+      <h1 className="text-5xl  font-bold text-center my-3">I Grow  By Helping People In Need</h1>
+      <div className="mt-10 text-center"> <input className="border p-2 px-3 rounded-md" type="text" placeholder="Serch here...." value="" />
         <button className="bg-[#FF444A] border p-2 px-3 rounded-md text-white">Serch</button>
       </div>
 
